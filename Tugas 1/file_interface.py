1540f424900ff7505c67382d5fae5442b209d74c
Lathifa Itqon <lathifaitqon@gmail.com>
2022-06-06 08:12:41 +0700

    feat: upload delete

---
+++ Tugas 1/file_interface.py
@@ -0,0 +1,59 @@
+import os
+import json
+import base64
+from glob import glob
+
+
+class FileInterface:
+    def __init__(self):
+        os.chdir('files/')
+
+    def list(self):
+        try:
+            filelist = glob('*.*')
+            return dict(status='OK',data=filelist)
+        except Exception as e:
+            return dict(status='ERROR',data=str(e))
+
+    def get(self,filename=''):
+        if(filename==''):
+            return None
+        try:
+            fp = open(f"{filename}",'rb')
+            isifile = base64.b64encode(fp.read()).decode()
+            return dict(status='OK',data_namafile=filename,data_file=isifile)
+        except Exception as e:
+            return dict(status='ERROR',data=str(e))
+
+    def upload(self, params=[]):
+        try:
+            filename = params[0]
+            if (os.path.exists(filename) == True):
+                return dict(status='ERROR',data=f'File {filename} sudah ada')
+            file = base64.b64decode(params[1])
+            
+            fp = open(filename,'wb+')
+            fp.write(file)
+            fp.close()
+            
+            return dict(status='OK',data=f'File {filename} berhasil terupload !')
+        except Exception as e:
+            return dict(status='ERROR',data=str(e))
+
+    def delete(self, params=[]):
+        try:
+            filename = params[0]
+            if (filename == ''):
+                return dict(status='ERROR',data='Parameter kosong!')
+            if (os.path.exists(filename) == False):
+                return dict(status='ERROR',data=f'File {filename} tidak ditemukan !')
+            os.remove(filename)
+            return dict(status='OK',data=f'File {filename} berhasil dihapus !')
+        except Exception as e:
+            return dict(status='ERROR',data=str(e))
+
+
+if __name__=='__main__':
+    f = FileInterface()
+    # print(f.list())
+    # print(f.get('pokijan.jpg'))
